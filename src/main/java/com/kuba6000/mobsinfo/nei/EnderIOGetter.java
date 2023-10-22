48ba04852e666284459e44dce4e7f48d7b412334
Jakub <53441451+kuba6000@users.noreply.github.com>
2023-10-22 17:11:25 +0200

    Add soul vial to usage handler (#55)
    * Add soul vial to usage handler
    
    * Cycle ingredients

--- src/main/java/com/kuba6000/mobsinfo/nei/EnderIOGetter.java
+++ src/main/java/com/kuba6000/mobsinfo/nei/EnderIOGetter.java
@@ -1,19 +1,35 @@
 package com.kuba6000.mobsinfo.nei;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import com.kuba6000.mobsinfo.mixin.EnderIO.BlockPoweredSpawnerAccessor;
 
 import crazypants.enderio.EnderIO;
+import crazypants.enderio.machine.spawner.BlockPoweredSpawner;
 
 class EnderIOGetter {
+
+    public static String getContainedMobOrNull(ItemStack stack) {
+        String mob = BlockPoweredSpawner.getSpawnerTypeFromItemStack(stack);
+        if (mob != null) return mob;
+        return EnderIO.itemSoulVessel.getMobTypeFromStack(stack);
+    }
 
     public static Block blockPoweredSpawner() {
         return EnderIO.blockPoweredSpawner;
     }
 
+    public static Item itemSoulVessel() {
+        return EnderIO.itemSoulVessel;
+    }
+
     public static ItemStack BlockPoweredSpawner$createItemStackForMob(String type) {
         return ((BlockPoweredSpawnerAccessor) EnderIO.blockPoweredSpawner).callCreateItemStackForMob(type);
+    }
+
+    public static ItemStack ItemSoulVessel$createVesselWithEntityStub(String entityId) {
+        return EnderIO.itemSoulVessel.createVesselWithEntityStub(entityId);
     }
 }
