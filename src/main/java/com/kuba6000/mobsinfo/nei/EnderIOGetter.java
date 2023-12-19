75b0715235f6d38f58f9fdd212b7d6ac26dadc48
Jakub <53441451+kuba6000@users.noreply.github.com>
2023-12-19 20:03:15 +0100

    Add broken spawner as a usage handler (#64)

--- src/main/java/com/kuba6000/mobsinfo/nei/EnderIOGetter.java
+++ src/main/java/com/kuba6000/mobsinfo/nei/EnderIOGetter.java
@@ -8,13 +8,14 @@ import com.kuba6000.mobsinfo.mixin.EnderIO.BlockPoweredSpawnerAccessor;
 
 import crazypants.enderio.EnderIO;
 import crazypants.enderio.machine.spawner.BlockPoweredSpawner;
+import crazypants.enderio.machine.spawner.ItemBrokenSpawner;
 
 class EnderIOGetter {
 
     public static String getContainedMobOrNull(ItemStack stack) {
         String mob = BlockPoweredSpawner.getSpawnerTypeFromItemStack(stack);
-        if (mob != null) return mob;
-        return EnderIO.itemSoulVessel.getMobTypeFromStack(stack);
+        if (mob == null) mob = EnderIO.itemSoulVessel.getMobTypeFromStack(stack);
+        return mob == null ? ItemBrokenSpawner.getMobTypeFromStack(stack) : mob;
     }
 
     public static Block blockPoweredSpawner() {
@@ -25,11 +26,19 @@ class EnderIOGetter {
         return EnderIO.itemSoulVessel;
     }
 
+    public static Item itemBrokenSpawner() {
+        return EnderIO.itemBrokenSpawner;
+    }
+
     public static ItemStack BlockPoweredSpawner$createItemStackForMob(String type) {
         return ((BlockPoweredSpawnerAccessor) EnderIO.blockPoweredSpawner).callCreateItemStackForMob(type);
     }
 
     public static ItemStack ItemSoulVessel$createVesselWithEntityStub(String entityId) {
         return EnderIO.itemSoulVessel.createVesselWithEntityStub(entityId);
+    }
+
+    public static ItemStack ItemBrokenSpawner$createStackForMobType(String mobType) {
+        return ItemBrokenSpawner.createStackForMobType(mobType);
     }
 }
