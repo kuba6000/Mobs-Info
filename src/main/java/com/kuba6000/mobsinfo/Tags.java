6a074618d6f2b7d7f9e993c25ea9083843d3d983
kuba6000 <kuba.123123.6000@gmail.com>
2023-12-29 00:02:41 +0100

    Use GRADLETOKEN_... only for versioning

--- src/main/java/com/kuba6000/mobsinfo/Tags.java
+++ src/main/java/com/kuba6000/mobsinfo/Tags.java
@@ -5,8 +5,8 @@ package com.kuba6000.mobsinfo;
 public class Tags {
 
     // GRADLETOKEN_* will be replaced by your configuration values at build time
-    public static final String MODID = "GRADLETOKEN_MODID";
-    public static final String MODNAME = "GRADLETOKEN_MODNAME";
+    public static final String MODID = "mobsinfo";
+    public static final String MODNAME = "MobsInfo";
     public static final String VERSION = "GRADLETOKEN_VERSION";
-    public static final String GROUPNAME = "GRADLETOKEN_GROUPNAME";
+    public static final String GROUPNAME = "com.kuba6000.mobsinfo";
 }
