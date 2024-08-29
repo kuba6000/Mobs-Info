6f958fa704ab11ee6475892d767a71a00b96a1dc
Jakub <53441451+kuba6000@users.noreply.github.com>
2024-08-29 22:20:43 +0200

    Add Villager Trades cache and API to provide Villager trades manually (#91)
    * Merge getModListVersion
    
    * Rename getModListVersion to getModListVersionForMobDrops
    
    * Cache Villager trades + IVillagerInfoProvider
    
    * Fix
    
    * helpers

--- src/main/java/com/kuba6000/mobsinfo/loader/MobRecipeLoader.java
+++ src/main/java/com/kuba6000/mobsinfo/loader/MobRecipeLoader.java
@@ -343,8 +343,8 @@ public class MobRecipeLoader {
 
         String modlistversion;
         if (Config.MobHandler.regenerationTrigger == Config.MobHandler._CacheRegenerationTrigger.ModAdditionRemoval)
-            modlistversion = ModUtils.getModListVersionIgnoringModVersions();
-        else modlistversion = ModUtils.getModListVersion();
+            modlistversion = ModUtils.getModListVersionForMobDrops(false);
+        else modlistversion = ModUtils.getModListVersionForMobDrops(true);
 
         isInGenerationProcess = true;
 
