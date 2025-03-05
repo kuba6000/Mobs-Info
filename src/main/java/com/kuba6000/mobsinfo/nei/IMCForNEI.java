726974878f4d3d901ee4c789019b538833388f2e
Jakub <53441451+kuba6000@users.noreply.github.com>
2025-03-05 21:45:04 +0100

    Fix gui height on larger gui scales (#109)

--- src/main/java/com/kuba6000/mobsinfo/nei/IMCForNEI.java
+++ src/main/java/com/kuba6000/mobsinfo/nei/IMCForNEI.java
@@ -35,13 +35,13 @@ public class IMCForNEI {
 
     public static void IMCSender() {
 
-        sendHandler("mobsinfo.mobhandler", "minecraft:diamond_sword", 168, 192, 1, 6);
+        sendHandler("mobsinfo.mobhandler", "minecraft:diamond_sword", 168, 105, 1, 6);
         sendCatalyst("mobsinfo.mobhandler", "minecraft:diamond_sword");
         if (LoaderReference.InfernalMobs.isLoaded) {
-            sendHandler("mobsinfo.mobhandlerinfernal", "minecraft:diamond_sword", 168, 192, 1, 6);
+            sendHandler("mobsinfo.mobhandlerinfernal", "minecraft:diamond_sword", 168, 105, 1, 6);
             sendCatalyst("mobsinfo.mobhandlerinfernal", "minecraft:diamond_sword");
         }
-        sendHandler("mobsinfo.villagertradeshandler", "minecraft:emerald", 168, 192, 1, 6);
+        sendHandler("mobsinfo.villagertradeshandler", "minecraft:emerald", 168, 105, 1, 6);
         sendCatalyst("mobsinfo.villagertradeshandler", "minecraft:emerald");
         sendCatalyst("mobsinfo.villagertradeshandler", "minecraft:spawn_egg:120");
 
